0658480cd4d4101737e3c9db8a5c99c477f2ba42
Noah Blattner <noahblattner04@gmail.com>
2022-12-10 11:32:44 +0100

    Amélioré le saut / Supprimé fichiers innutiles

--- Assets/Scripts/Mouton/Mouvement.cs
+++ Assets/Scripts/Mouton/Mouvement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
+
+    // Paramètres
     [SerializeField]
     private int walkSpeed = 10;
-
     [SerializeField]
     [Range(0, 1000)]
-    private int jumpForce = 10;
+    private int jumpHeight = 100;
+    [SerializeField]
+    private float jumpDelay = 0.1f;
+
+    // Variables
+    private float groundTime = 0;
     Rigidbody2D rigidbody2DMouton;
+    private bool jumping = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,23 +28,37 @@ public class Mouvement : MonoBehaviour
     // Update is called once per frame
     void Update()  
     {
+        // Déplacement sur l'axe x
         float inputX = Input.GetAxis("Horizontal");
-        
         rigidbody2DMouton.velocity = new Vector2(inputX * walkSpeed,rigidbody2DMouton.velocity.y);
 
+        // Atterissage
+        if (jumping && isGrounded())
+        {
+            groundTime += Time.deltaTime;
+            if (groundTime > jumpDelay)
+            {
+                groundTime = 0;
+                jumping = false;
+            }
+        }
+
+        if (!jumping && Input.GetAxis("Jump") != 0 && isGrounded()) {
+            jumping = true;
+            rigidbody2DMouton.AddForce(new Vector2(0, jumpForce()));
+        }
+    }
+
+    private bool isGrounded() {
         Vector2 lowerCenter = transform.position;
         lowerCenter.y -= transform.localScale.y/2f;
         Vector2 lowerLeft = new Vector2(lowerCenter.x - (transform.localScale.x + 0.1f), lowerCenter.y);
         Vector2 lowerRight = new Vector2(lowerCenter.x + (transform.localScale.x + 0.1f), lowerCenter.y);
-        
-        float jump = Input.GetAxis("Jump");
-    
-        Debug.DrawLine(lowerLeft, new Vector2(lowerLeft.x, lowerLeft.y-0.1f), Color.red, 1f);
-        Debug.DrawLine(lowerRight, new Vector2(lowerRight.x, lowerRight.y-0.1f), Color.red, 1f);
-        if (jump != 0) {
-            if (Physics2D.Raycast(lowerLeft, Vector2.down, 0.05f) || Physics2D.Raycast(lowerRight, Vector2.down, 0.05f)) {
-                rigidbody2DMouton.AddForce(new Vector2(0, Mathf.Sqrt(-2 * Physics2D.gravity.y * jumpForce)));
-            }
-        }
+        return Physics2D.Raycast(lowerLeft, Vector2.down, 0.1f) || Physics2D.Raycast(lowerRight, Vector2.down, 0.1f);
+    }
+
+    private float jumpForce() {
+        float adaptedJumpHeight = jumpHeight * 100;
+        return Mathf.Sqrt(-2 * Physics2D.gravity.y * adaptedJumpHeight * rigidbody2DMouton.gravityScale);
     }
 }
